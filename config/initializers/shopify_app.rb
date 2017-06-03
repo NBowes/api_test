67901138e45025b45db7b9be61bf0e8687efd69e
NBowes <nathen.bowes@shopify.com>
2017-06-03 10:09:02 -0700

    wip

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,7 +1,7 @@
 ShopifyApp.configure do |config|
   config.application_name = "My Shopify App"
-  config.api_key = ENV['API_KEY']
-  config.secret = ENV['API_SECRET']
+  config.api_key = "50bfb9d68313b6f46f0e94558169b2cd"
+  config.secret = "ad1a6aeeb88bd32ee138d5dbab213a81"
   config.scope = "read_orders, read_products"
   config.embedded_app = true
 end
